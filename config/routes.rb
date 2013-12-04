a1d81d6c8546554008d530cae8a58d7e9727cec2
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-04 19:00:57 +0400

    aa

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Arteslav::Application.routes.draw do
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
 
   root :to => 'static_pages#index'
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 end
