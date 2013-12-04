ca15da701557932e1de2b1d5549171a87f0d7667
Viacheslav Bobrov <babrovka@gmail.com>
2013-12-04 18:39:12 +0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'sqlite3'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'capistrano'
+gem 'rvm-capistrano'
+gem 'execjs'
+gem 'therubyracer', '~> 0.10.2'
+
+gem "paperclip", "~> 3.0"
+gem "russian", "~> 0.6.0"
+
+gem 'activeadmin'
+
+gem 'jquery-rails', '~> 2.3.0'
+gem 'jquery-ui-rails'
+
+group :production do
+  gem 'thin'
+  gem 'mysql2'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Arteslav::Application.config.secret_token = 'dd7fd8c58162123d3d0da22c933e7eba272acf629162e121bf4a2e4f8260f3c5720b6d9f238e5d00bf533c47ec3aab98158ceedc406b5491ab9be63600f16545'
